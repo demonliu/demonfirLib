d6f9931673a8c61799c6f77f835ed75b152f9739
demonliu <1432694660@qq.com>
2017-05-28 21:52:54 +0800

    firstmsg

---
+++ demonfirLib/Classes/demonObj.h
@@ -0,0 +1,13 @@
+//
+//  demonObj.h
+//  Pods
+//
+//  Created by Liumengyao on 2017/5/28.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface demonObj : NSObject
+
+@end
---
+++ demonfirLib/Classes/demonObj.m
@@ -0,0 +1,13 @@
+//
+//  demonObj.m
+//  Pods
+//
+//  Created by Liumengyao on 2017/5/28.
+//
+//
+
+#import "demonObj.h"
+
+@implementation demonObj
+
+@end
